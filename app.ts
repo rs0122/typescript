165356d77935dbf31df1cfb765b1d8e85b2c5a2b
rs0122 <rs.0122.ssthe2@hotmail.co.jp>
2023-02-11 16:28:34 +0900

    変数・データ型の基本

--- app.ts
+++ app.ts
@@ -2,3 +2,17 @@ console.log('Hello World!');
 
 const str: string = 'Hello';
 console.log(str);
+
+let color: string = 'Red';
+console.log(color);
+console.log(typeof(color));
+
+const pi = 3.14;
+console.log(pi);
+
+const firstName = 'Ryunosuke';
+const lastName = 'Saito';
+
+// テンプレートリテラル
+const message = `Hello! I am ${firstName} ${lastName}.`;
+console.log(message);
