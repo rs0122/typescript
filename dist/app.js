e3dd775d4a5a306922a19361e3120696683ee1ac
rs0122 <rs.0122.ssthe2@hotmail.co.jp>
2023-02-12 20:13:46 +0900

    コンパイル設定

---
+++ dist/app.js
@@ -0,0 +1,15 @@
+"use strict";
+console.log('Hello World!');
+const str = 'Hello';
+console.log(str);
+let color = 'Red';
+console.log(color);
+console.log(typeof (color));
+const pi = 3.14;
+console.log(pi);
+const firstName = 'Ryunosuke';
+const lastName = 'Saito';
+// テンプレートリテラル
+const message = `Hello! I am ${firstName} ${lastName}.`;
+console.log(message);
+//# sourceMappingURL=app.js.map
---
+++ dist/second.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log('Hello second.ts!!!');
+//# sourceMappingURL=second.js.map
