b677e5c5f254eb3c2f1bc242eeb8daa0ba3ed56f
rs0122 <rs.0122.ssthe2@hotmail.co.jp>
2023-03-21 15:21:31 +0900

    オブジェクト操作

--- src/app.ts
+++ src/app.ts
@@ -25,45 +25,94 @@
 // console.log(rectangle(3, 5));
 
 // 配列
-const colors: string[] = ['Red', 'Green', 'Blue'];
+// const colors: string[] = ['Red', 'Green', 'Blue'];
 // console.log(colors);
 // console.log(colors.length);
 
-colors.push('Black');
-console.log(colors);
-
-colors.unshift('white');
-console.log(colors);
-
-colors[1] = 'Aka';
-console.log(colors);
+// colors.push('Black');
+// console.log(colors);
+// 
+// colors.unshift('white');
+// console.log(colors);
+// 
+// colors[1] = 'Aka';
+// console.log(colors);
 
 // 追加
-const insertArr = ['a', 'b', 'c'];
-insertArr.splice(1, 0, 'D');
-insertArr.splice(2, 0, 'X', 'Y', 'Z');
-console.log(insertArr);
+// const insertArr = ['a', 'b', 'c'];
+// insertArr.splice(1, 0, 'D');
+// insertArr.splice(2, 0, 'X', 'Y', 'Z');
+// console.log(insertArr);
 
 // 削除
-insertArr.splice(2, 2);
-console.log(insertArr);
-
-const arr1 = ['a', 'b', 'c'];
-const arr2 = ['d', 'e', 'f'];
+// insertArr.splice(2, 2);
+// console.log(insertArr);
+// 
+// const arr1 = ['a', 'b', 'c'];
+// const arr2 = ['d', 'e', 'f'];
 // 元の配列に変更なし
-const mergedArr = arr1.concat(arr2);
-console.log(mergedArr);
+// const mergedArr = arr1.concat(arr2);
+// console.log(mergedArr);
 
 // 先頭を削除
-const arr3 = ['a', 'b', 'c'];
-arr3.shift();
-console.log(arr3);
+// const arr3 = ['a', 'b', 'c'];
+// arr3.shift();
+// console.log(arr3);
 
 // 末尾を削除
-arr3.pop();
-console.log(arr3);
+// arr3.pop();
+// console.log(arr3);
+// 
+// const arr4 = [1, 2, 3];
+// const arr5 = [4, 5, 6, ...arr4];
+// console.log(arr5);
 
-const arr4 = [1, 2, 3];
-const arr5 = [4, 5, 6, ...arr4];
-console.log(arr5);
+// オブジェクト
+const user = {
+    name : 'Ryunosuke Saito',
+    gender : 'man',
+    age : 29
+};
 
+console.log(user.name);
+console.log(user.gender);
+console.log(user.age);
+
+console.log(user['name']);
+console.log(user['gender']);
+console.log(user['age']);
+
+user.age = 30;
+console.log(user.age);
+
+// プロパティの個数を求める
+console.log(Object.keys(user).length);
+console.log(Object.keys(user));
+
+// 型定義
+const user2: {
+    name:string,
+    gender:string,
+    age:number
+} = {
+    name: 'Ryunosuke Saito',
+    gender: 'man',
+    age: 29
+};
+
+// インターフェイス
+interface UserObj {
+    name:string,
+    gender:string,
+    age?:number
+}
+
+const user3: UserObj = {
+    name: 'Ryunosuke Saito',
+    gender: 'man',
+    age: 29
+}
+
+console.log(user3.name);
+console.log(user3.gender);
+console.log(user3.age);
