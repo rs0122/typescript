6184ee75e2f695013dba491e8a37fbfb600f36a1
rs0122 <rs.0122.ssthe2@hotmail.co.jp>
2023-04-01 15:37:57 +0900

    オプショナルなプロパティの使用

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,24 @@
 "use strict";
-class User {
-    name = '';
-    age = 0;
-}
-const olivia = new User();
-console.log(olivia.name);
-console.log(olivia.age);
-olivia.name = 'Olivia';
-olivia.age = 23;
-console.log(olivia.name);
-console.log(olivia.age);
+// class User{
+//     name: string = '';
+//     age: number = 0;
+// }
+// const olivia = new User();
+// console.log(olivia.name);
+// console.log(olivia.age);
+// olivia.name = 'Olivia';
+// olivia.age = 23;
+// console.log(olivia.name);
+// console.log(olivia.age);
+// オプションナルなプロパティ
+// class User{
+// name?: string;
+// age: number = 0;
+// }
+// 
+// const olivia = new User();
+// console.log(olivia.name);//undefinedで出力
+// console.log(olivia.age);
+// 
+// olivia.name = 'Olivia';
+// console.log(olivia.name);
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,26 @@
-class User{
-    name: string = '';
-    age: number = 0;
-}
+// class User{
+//     name: string = '';
+//     age: number = 0;
+// }
 
-const olivia = new User();
-console.log(olivia.name);
-console.log(olivia.age);
+// const olivia = new User();
+// console.log(olivia.name);
+// console.log(olivia.age);
 
-olivia.name = 'Olivia';
-olivia.age = 23;
-console.log(olivia.name);
-console.log(olivia.age);
+// olivia.name = 'Olivia';
+// olivia.age = 23;
+// console.log(olivia.name);
+// console.log(olivia.age);
+
+// オプションナルなプロパティ
+// class User{
+    // name?: string;
+    // age: number = 0;
+// }
+// 
+// const olivia = new User();
+// console.log(olivia.name);//undefinedで出力
+// console.log(olivia.age);
+// 
+// olivia.name = 'Olivia';
+// console.log(olivia.name);
